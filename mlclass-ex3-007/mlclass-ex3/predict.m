be33bdb2da2b347148e9b21076540e57e896eec7
Remi Gabillet <rgabillet@gmail.com>
2014-11-01 15:44:11 -0700

    ex3 + Lecture 9

--- mlclass-ex3-007/mlclass-ex3/predict.m
+++ mlclass-ex3-007/mlclass-ex3/predict.m
@@ -1,35 +1,41 @@
 function p = predict(Theta1, Theta2, X)
-%PREDICT Predict the label of an input given a trained neural network
-%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
-%   trained weights of a neural network (Theta1, Theta2)
 
-% Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+	%PREDICT Predict the label of an input given a trained neural network
+	%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+	%   trained weights of a neural network (Theta1, Theta2)
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+	% Useful values
+	m = size(X, 1);
+	num_labels = size(Theta2, 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
+	% You need to return the following variables correctly 
+	p = zeros(size(X, 1), 1);
 
-
-
+	% ====================== YOUR CODE HERE ======================
+	% Instructions: Complete the following code to make predictions using
+	%               your learned neural network. You should set p to a 
+	%               vector containing labels between 1 to num_labels.
+	%
+	% Hint: The max function might come in useful. In particular, the max
+	%       function can also return the index of the max element, for more
+	%       information see 'help max'. If your examples are in rows, then, you
+	%       can use max(A, [], 2) to obtain the max for each row.
+	%
 
 
 
+	% Add ones to the X data matrix
+	X = [ones(m, 1) X];
+	% newX = [ones(m, 1) X];
+
+	layer2_predictions = sigmoid(X * Theta1');
 
 
+	X2 = [ones(m, 1) layer2_predictions];
+	layer3_predictions = sigmoid(X2 * Theta2');
 
-% =========================================================================
-
+	[ layer3_max_value, layer3_max_index ] = max(layer3_predictions, [], 2);
+	p = layer3_max_index;
+	% =========================================================================
 
 end
